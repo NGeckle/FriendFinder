ac09cad1fce3ab3ec25cece6642ceec7ade0180e
Nathan Geckle <nathan_geckle@yahoo.com>
2018-04-28 11:03:16 -0400

    Npm Update

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,11 +4,11 @@ module.exports = function(app) {
 
     var PORT = process.env.PORT || 8000;
 
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
