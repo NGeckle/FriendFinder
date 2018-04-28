060c32032b82dc7ec139fb5809d4fc49e18bbd12
Nathan Geckle <nathan_geckle@yahoo.com>
2018-04-28 18:27:50 -0400

    Fixed locations of files

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,4 @@
 var friends = [];
 
+
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var path = require("path");
 
-var friends = require("../app/data/friends.js");
+var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
@@ -8,9 +8,6 @@ module.exports = function(app) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
-        if (friends.data < 5) {
-            friends.push(req.body);
-            res.json(true);
-        } 
+        friends.push(req.body);
     });
 };
