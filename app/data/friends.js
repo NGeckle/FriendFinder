a6c047b63d22048f8b6979b0117314c5b120f7bb
Nathan Geckle <nathan_geckle@yahoo.com>
2018-04-28 21:54:21 -0400

    Got it working. Next up styling.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,10 @@
-var friends = [];
+var friends = [
+    {
+        name: "Negative Nelson",
+        photo: "https://pbs.twimg.com/profile_images/2291477315/Real-Life-Rage-Comic-Rage-Faces-Memes_400x400.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
 
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,29 @@ module.exports = function(app) {
         res.json(friends);
     });
     app.post("/api/friends", function(req, res) {
+        var difference = 40;
+        var matchName = "";
+        var matchPhoto = "";
+        friends.forEach(function(friend) {
+            var matchedScores = [];
+            var totalDifference = 40;
+            function add(a, b) {
+                return a + b;
+            }
+            for (var i = 0; i < friend.scores.length; i++) {
+                matchedScores.push(Math.abs(parseInt(req.body.scores[i])) - parseInt(friend.scores[i]));
+            }
+            totalDifference = matchedScores.reduce(add, 0);
+            if (totalDifference < difference) {
+                difference = totalDifference;
+                matchName = friend.name;
+                matchPhoto = friend.photo;
+            }
+        });
+        res.json({
+            name: matchName,
+            photo: matchPhoto
+        });
         friends.push(req.body);
     });
 };
